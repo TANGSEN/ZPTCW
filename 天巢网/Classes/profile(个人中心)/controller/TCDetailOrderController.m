716365612ce840c18bedb8dbf84144fbdd728a48
tjp <tangjianpingIT@126.com>
2015-11-18 23:11:10 +0800

    修改了订单详情视图不能完全展示的bug，

--- 天巢网/Classes/profile(个人中心)/controller/TCDetailOrderController.m
+++ 天巢网/Classes/profile(个人中心)/controller/TCDetailOrderController.m
@@ -38,7 +38,8 @@
     self.AddressLabel.font = AppFont(text_size_other);
     self.AddressLabel.textColor = Color_Common;
     
-    self.scrollV = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, ApplicationframeValue.size.width, ApplicationframeValue.size.height)];
+    // navigation遇到scrollV会自动把scrollV的y值设置成64，所以要想让scrollV显示完全要把scrollV的height减掉一部分，这里又有一个付款按钮会挡住一部分scrollV，所以还要减去付款按钮的height，这样就能把scrollV完全显示了。
+    self.scrollV = [[UIScrollView alloc] initWithFrame:CGRectMake(0, 0, ApplicationframeValue.size.width, ApplicationframeValue.size.height - 50 - 55)];
     self.scrollV.contentSize = CGSizeMake(ApplicationframeValue.size.width, 154+411+20);
     self.scrollV.backgroundColor =[UIColor clearColor];
     [self.view addSubview:self.scrollV];
@@ -49,12 +50,32 @@
     self.Table.frame = CGRectMake(0, 164, ApplicationframeValue.size.width, 411);
     [self.scrollV addSubview:self.Table];
     
-   
+    
+    
+    
+    /*********************************************************************/
+    // blockskit功能小试
+    UIButton *btn = [[UIButton alloc]init];
+    btn.x = 0;
+    btn.y = 300;
+    btn.Width = JPScreenW;
+    btn.height = 120;
+    btn.backgroundColor = RandomColor;
+    btn.titleLabel.numberOfLines = 0;
+    [btn setTitle:@"***********这是一个按钮**********\n使用blockskit框架把[btn addTarget:self action:@selector(btnClick) forControlEvents:UIControlEventTouchUpInside]用block实现" forState:UIControlStateNormal];
+//    [btn addTarget:self action:@selector(btnClick) forControlEvents:UIControlEventTouchUpInside];
+    [btn bk_addEventHandler:^(id sender) {
+        NSLog(@"点击了btn按钮，使用blockskit框架把[btn addTarget:self action:@selector(btnClick) forControlEvents:UIControlEventTouchUpInside]用block实现");
+    } forControlEvents:UIControlEventTouchUpInside];
+    [self.view addSubview:btn];
+    /*********************************************************************/
+    
     
     
     
     [self getRoundCorner:self.StepperView];
-    
+    self.scrollV.bounces = NO;
+    NSLog(@"%@",NSStringFromCGRect(self.scrollV.frame));
 }
 
 -(UIView *)getRoundCorner:(UIView *)vView
